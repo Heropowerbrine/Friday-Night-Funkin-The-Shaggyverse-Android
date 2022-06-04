3b873a442fe1a73043b55062716722348b4f287a
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-04 14:34:27 +0800

    Update ModchartUtilities.hx

--- source/modding/ModchartUtilities.hx
+++ source/modding/ModchartUtilities.hx
@@ -173,7 +173,7 @@ class ModchartUtilities
         Lua.init_callbacks(lua);
 
         if(path == null)
-            path = PolymodAssets.getPath(Paths.lua("modcharts/" + PlayState.SONG.modchartPath));
+            path = Assets.getPath(Paths.lua("modcharts/" + PlayState.SONG.modchartPath));
 
         var result = LuaL.dofile(lua, path); // execute le file
 
