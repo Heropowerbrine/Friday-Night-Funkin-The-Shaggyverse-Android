c9f2b3de32b7c755b46ac424af117cb1360c3602
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-05 15:34:23 +0800

    need to check stuff here later

--- source/states/MusicBeatState.hx
+++ source/states/MusicBeatState.hx
@@ -33,6 +33,108 @@ class MusicBeatState extends FlxUIState
 
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
+	
+	#if android
+	var _virtualpad:FlxVirtualPad;
+	var _hitbox:FlxHitbox;
+	var trackedinputsUI:Array<FlxActionInput> = [];
+	var trackedinputsNOTES:Array<FlxActionInput> = [];
+	#end
+	
+	#if android
+	public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+		_virtualpad = new FlxVirtualPad(DPad, Action, 0.75, ClientPrefs.globalAntialiasing);
+		add(_virtualpad);
+		controls.setVirtualPadUI(_virtualpad, DPad, Action);
+		trackedinputsUI = controls.trackedinputsUI;
+		controls.trackedinputsUI = [];
+	}
+	#end
+
+	#if android
+	public function removeVirtualPad() {
+		controls.removeFlxInput(trackedinputsUI);
+		remove(_virtualpad);
+	}
+	#end
+
+	#if android
+	public function addHitbox(mania:Int) {               
+		var curhitbox:HitboxType = FOUR;
+
+		switch (mania){
+			case 1:
+				curhitbox = ONE;
+			case 2:
+				curhitbox = TWO;
+			case 3:
+				curhitbox = THREE;					
+			case 4:
+				curhitbox = FOUR;	
+			case 5:
+				curhitbox = FIVE;
+			case 6:
+				curhitbox = SIX;
+			case 7:
+				curhitbox = SEVEN;
+			case 8:
+				curhitbox = EIGHT;
+			case 9:
+				curhitbox = NINE;
+			case 10:
+				curhitbox = TEN;
+		        case 11:
+				curhitbox = ELEVEN;	
+			case 12:
+				curhitbox = TWELVE;
+			case 13:
+				curhitbox = THIRTEEN;
+			case 14:
+				curhitbox = FOURTEEN;
+			case 15:
+				curhitbox = FIFTEEN;
+			case 16:
+				curhitbox = SIXTEEN;
+			case 17:
+				curhitbox = SEVENTEEN;
+			case 18:
+				curhitbox = EIGHTEEN;
+			case 19:
+				curhitbox = NINETEEN;
+			case 20:
+				curhitbox = TWENTY;
+			case 21:
+				curhitbox = TWENTYONE;
+			default:
+				curhitbox = FOUR;
+		}
+
+		_hitbox = new FlxHitbox(curhitbox, 0.75, ClientPrefs.globalAntialiasing);
+
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_hitbox.cameras = [camcontrol];
+
+		_hitbox.visible = false;
+		add(_hitbox);
+	}
+	#end
+
+	#if android
+        public function addPadCamera() {
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_virtualpad.cameras = [camcontrol];
+	}
+	#end
+	
+	override function destroy() {
+		#if android
+		controls.removeFlxInput(trackedinputsUI);
+		controls.removeFlxInput(trackedinputsNOTES);	
+		#end
 
 	override function update(elapsed:Float)
 	{
