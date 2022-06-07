e5e4c0a1bf38594c9f09a22dc53489ffd7414d3a
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-07 18:20:16 +0800

    Update HitboxSubstate.hx

--- source/substates/HitboxSubstate.hx
+++ source/substates/HitboxSubstate.hx
@@ -3,7 +3,7 @@ package substates;
 import android.flixel.FlxHitbox;
 import states.PlayState;
 import states.MusicBeatState;
-import states.MusicBeatSubstate;
+import substates.MusicBeatSubstate;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;
@@ -15,6 +15,68 @@ class HitboxSubstate extends MusicBeatSubstate
   override public function create()
   {
    var ogPlayerKeyCount = PlayState.ogPlayerKeyCount;
+   var _hitbox = FlxHitbox;
+   public function addHitbox(ogPlayerKeyCount:Int) {               
+		var curhitbox:HitboxType = FOUR;
+
+		switch (ogPlayerKeyCount){
+			case 1:
+				curhitbox = ONE;
+			case 2:
+				curhitbox = TWO;
+			case 3:
+				curhitbox = THREE;					
+			case 4:
+				curhitbox = FOUR;	
+			case 5:
+				curhitbox = FIVE;
+			case 6:
+				curhitbox = SIX;
+			case 7:
+				curhitbox = SEVEN;
+			case 8:
+				curhitbox = EIGHT;
+			case 9:
+				curhitbox = NINE;
+			case 10:
+				curhitbox = TEN;
+		        case 11:
+				curhitbox = ELEVEN;	
+			case 12:
+				curhitbox = TWELVE;
+			case 13:
+				curhitbox = THIRTEEN;
+			case 14:
+				curhitbox = FOURTEEN;
+			case 15:
+				curhitbox = FIFTEEN;
+			case 16:
+				curhitbox = SIXTEEN;
+			case 17:
+				curhitbox = SEVENTEEN;
+			case 18:
+				curhitbox = EIGHTEEN;
+			case 19:
+				curhitbox = NINETEEN;
+			case 20:
+				curhitbox = TWENTY;
+			case 21:
+				curhitbox = TWENTYONE;
+			default:
+				curhitbox = FOUR;
+		}
+
+		_hitbox = new FlxHitbox(curhitbox);
+
+		var camcontrol = new flixel.FlxCamera();
+		FlxG.cameras.add(camcontrol);
+		camcontrol.bgColor.alpha = 0;
+		_hitbox.cameras = [camcontrol];
+
+		_hitbox.visible = false;
+		add(_hitbox);
+	}
+    
    if(PlayState.startedCountdown = true)
      addHitbox(ogPlayerKeyCount);
      _hitbox.visible = true;
