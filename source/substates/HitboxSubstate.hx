8533a281acade03870c46fb28140b065a634056c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-07 12:08:21 +0800

    Update HitboxSubstate.hx

--- source/substates/HitboxSubstate.hx
+++ source/substates/HitboxSubstate.hx
@@ -8,9 +8,12 @@ import flixel.FlxSprite;
 import flixel.FlxCamera;
 import flixel.util.FlxColor;
 
-class HitboxSubstate extends MusicBeatSubstate
-{
+class HitboxSubstate extends MusicBeatSubstate && MusicBeatState
+{ 
+  public function new
+ {
   if(PlayState.startedCountdown = true)
     addHitbox(ogPlayerKeyCount);
     _hitbox.visible = true;
+ }
 }
