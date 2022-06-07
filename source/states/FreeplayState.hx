b3ef52da862f706a0dfa561ebfea6b3568a77ef2
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-07 12:54:06 +0800

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -410,7 +410,7 @@ class FreeplayState extends MusicBeatState
 				vocals.volume = 0.7;
 			}
 
-			if(vocals != null && FlxG.sound.music != null && !FlxG.keys.justPressed.ENTER #if android || _virtualpad.buttonA.justPressed #end)
+			if(vocals != null && FlxG.sound.music != null && !controls.ACCEPT)
 			{
 				if(vocals.active && FlxG.sound.music.active)
 				{
@@ -432,10 +432,10 @@ class FreeplayState extends MusicBeatState
 			#if cpp
 			@:privateAccess
 			{
-				if(FlxG.sound.music.active && FlxG.sound.music.playing && !FlxG.keys.justPressed.ENTER)
+				if(FlxG.sound.music.active && FlxG.sound.music.playing && !controls.ACCEPT)
 					lime.media.openal.AL.sourcef(FlxG.sound.music._channel.__source.__backend.handle, lime.media.openal.AL.PITCH, curSpeed);
 	
-				if(vocals.active && vocals.playing && !FlxG.keys.justPressed.ENTER #if android || _virtualpad.buttonA.justPressed #end)
+				if(vocals.active && vocals.playing && !controls.ACCEPT)
 					lime.media.openal.AL.sourcef(vocals._channel.__source.__backend.handle, lime.media.openal.AL.PITCH, curSpeed);
 			}
 			#end
@@ -447,7 +447,7 @@ class FreeplayState extends MusicBeatState
 				changeSelection();
 			}
 
-			if(FlxG.keys.justPressed.ENTER && canEnterSong)
+			if(controls.ACCEPT && canEnterSong)
 			{
 				var poop:String = Highscore.formatSong(songs[curSelected].songName.toLowerCase(), curDiffString);
 	
