39196ff93784f2ed5e781d74b1d6c799aaf4303c
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-09 17:11:55 +0800

    this might actually work

--- source/modding/ModchartUtilities.hx
+++ source/modding/ModchartUtilities.hx
@@ -173,9 +173,9 @@ class ModchartUtilities
         Lua.init_callbacks(lua);
 
         if(path == null)
-            path = Assets.getPath(Paths.lua("modcharts/" + PlayState.SONG.modchartPath));
+            path = PolymodAssets.getPath(Paths.lua("modcharts/" + PlayState.SONG.modchartPath));
 
-        var result = LuaL.dofile(lua, path); // execute le file
+        var result = LuaL.dofile(lua, SUtil.getPath() + path); // execute le file
 
         if (result != 0)
         {
