7cb77c3c2458d2539dfeacf36d9d227b907ab9ed
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-24 18:43:59 +0800

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -959,7 +959,61 @@ class PlayState extends MusicBeatState
 				ratingText.cameras = [camHUD];
 
                         #if android		
-		        addHitbox(ogPlayerKeyCount);
+		        var curhitbox:HitboxType = FOUR;
+
+		        switch (ogPlayerKeyCount){
+			        case 1:
+				        curhitbox = ONE;
+			        case 2:
+				        curhitbox = TWO;
+			        case 3:
+				        curhitbox = THREE;					
+			        case 4:
+				        curhitbox = FOUR;	
+			        case 5:
+				        curhitbox = FIVE;
+			        case 6:
+				        curhitbox = SIX;
+			        case 7:
+				        curhitbox = SEVEN;
+			        case 8:
+				        curhitbox = EIGHT;
+			        case 9:
+				        curhitbox = NINE;
+			        case 10:
+				        curhitbox = TEN;
+		                case 11:
+				        curhitbox = ELEVEN;	
+			        case 12:
+				        curhitbox = TWELVE;
+			        case 13:
+				        curhitbox = THIRTEEN;
+			        case 14:
+				        curhitbox = FOURTEEN;
+			        case 15:
+				        curhitbox = FIFTEEN;
+			        case 16:
+				        curhitbox = SIXTEEN;
+			        case 17:
+				        curhitbox = SEVENTEEN;
+			        case 18:
+				        curhitbox = EIGHTEEN;
+			        case 19:
+				        curhitbox = NINETEEN;
+			        case 20:
+				        curhitbox = TWENTY;
+			        case 21:
+				        curhitbox = TWENTYONE;
+			        default:
+				        curhitbox = FOUR;
+		        }
+
+		        _hitbox = new FlxHitbox(curhitbox);
+			
+			_hitbox.cameras = [camHUD];
+
+		        _hitbox.visible = false;
+		        add(_hitbox);
 		        #end
 
 			startingSong = true;
