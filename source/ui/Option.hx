e19b3a02bff879c82d8b5e894e7a3e4613a8f0bc
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-06-25 17:03:31 +0800

    Update Option.hx

--- source/ui/Option.hx
+++ source/ui/Option.hx
@@ -24,6 +24,15 @@ import flixel.group.FlxGroup;
 import utilities.Controls;
 import substates.MusicBeatSubstate;
 import states.MusicBeatState;
+import flixel.input.FlxInput;
+import flixel.input.actions.FlxAction;
+import flixel.input.actions.FlxActionInput;
+import flixel.input.actions.FlxActionInputDigital;
+import flixel.input.actions.FlxActionManager;
+import flixel.input.actions.FlxActionSet;
+import flixel.input.gamepad.FlxGamepadButton;
+import flixel.input.gamepad.FlxGamepadInputID;
+import flixel.input.keyboard.FlxKey;
 
 /**
  * The base option class that all options inherit from.
