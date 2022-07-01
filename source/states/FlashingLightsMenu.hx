01205ec2d1bd564a7c5bdc85f885999cf8d3bfa0
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-01 10:45:29 +0800

    Update FlashingLightsMenu.hx

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -14,7 +14,7 @@ class FlashingLightsMenu extends MusicBeatState
         #end
         super.create();
 
-        var text = new FlxText(0,0,0,"Hey! Thank you for choosing The Shaggyverse\n Stuff may break!!!\nIf anything breaks, message VortexDev#2258 or Sebastian Elijah#4898\non Discord and tell us what broke.\nP.S.: This mod contains flashing lights!\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)", 32);        text.font = Paths.font("vcr.ttf");
+        var text = new FlxText(0,0,0,"Hey! Thank you for choosing The Shaggyverse\n Stuff may break!!!\nIf anything breaks, message VortexDev#2258 or Sebastian Elijah#4898\non Discord and tell us what broke.(For the Android port, contact Heropowerbrine#4019)\nP.S.: This mod contains flashing lights!\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)", 32);        text.font = Paths.font("vcr.ttf");
         text.screenCenter();
         text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
         add(text);
