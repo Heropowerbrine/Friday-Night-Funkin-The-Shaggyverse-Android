c9d69f9e7ff955111df16f8c685b2332fac3f4c3
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2022-07-07 15:38:15 +0800

    Update FlashingLightsMenu.hx

--- source/states/FlashingLightsMenu.hx
+++ source/states/FlashingLightsMenu.hx
@@ -10,11 +10,11 @@ class FlashingLightsMenu extends MusicBeatState
     override public function create()
     {
         #if android
-	    addVirtualPad(NONE, A);
+	    addVirtualPad(NONE, A_B);
         #end
         super.create();
 
-        var text = new FlxText(0,0,0,"Hey! Thank you for choosing The Shaggyverse\n Stuff may break!!!\nIf anything breaks, message VortexDev#2258 or Sebastian Elijah#4898\non Discord and tell us what broke.\n (For the Android port, contact Heropowerbrine#4019)\nP.S.: This mod contains flashing lights!\nPress Y to enable them, or anything else to not.\n(Any key closes this menu)", 32);        text.font = Paths.font("vcr.ttf");
+        var text = new FlxText(0,0,0,"Hey! Thank you for choosing The Shaggyverse\n Stuff may break!!!\nIf anything breaks, message VortexDev#2258 or Sebastian Elijah#4898\non Discord and tell us what broke.\n (For the Android port, contact Heropowerbrine#4019)\nP.S.: This mod contains flashing lights!\nPress A to enable them, or B to not.\n(Any key closes this menu)", 32);        text.font = Paths.font("vcr.ttf");
         text.screenCenter();
         text.setBorderStyle(OUTLINE, FlxColor.BLACK, 1.5, 1);
         add(text);
@@ -24,12 +24,12 @@ class FlashingLightsMenu extends MusicBeatState
     {
         super.update(elapsed);
 
-        if(FlxG.keys.justPressed.Y)
+        if(controls.ACCEPT)
             Options.setData(true, "flashingLights");
-        else if(!FlxG.keys.justPressed.Y && controls.ACCEPT)
+        else if(!controls.ACCEPT && controls.BACK)
             Options.setData(false, "flashingLights");
 
-        if(controls.ACCEPT)
+        if(controls.BACK)
             FlxG.switchState(new TitleState());
     }
 }
